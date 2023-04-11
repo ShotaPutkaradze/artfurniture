b7b8ebf10bf80b5d2789346185686becd5587b63
putkaradzeshot@gmail.com <putkaradzeshot@gmail.com>
2023-04-11 23:37:31 +0400

    start header

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,5 +1,22 @@
+import styles from "./Header.module.css";
+
 const Header = () => {
-  return <div>HEADER</div>;
+  return (
+    <div className={styles.header_container}>
+      <div>
+        <h1>davit@artfurniture.ge</h1>
+      </div>
+      <div>
+        <ul>
+          <li>მთავარი</li>
+          <li>ჩვენს შესახებ</li>
+          <li>მომსახურეობა</li>
+          <li>პროექტები</li>
+          <li>კონტაქტი</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
